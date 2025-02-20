2197b148ad2236440f11718c2c8c7c218c22c636
류대현 <eogus1027@naver.com>
2025-02-20 15:16:14 +0900

    프로젝트 생성

---
+++ src/main/java/org/example/springauthsample/SpringAuthSampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springauthsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springauthsample/SpringAuthSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springauthsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
